f6df7d237c0e7f6d1ec92a22b70d40a47c7e410a
ronak083 <ronakgupta083@gmail.com>
2023-07-29 23:43:10 +0530

    update 1

--- app/src/main/java/com/learn/bookhub/MainActivity.kt
+++ app/src/main/java/com/learn/bookhub/MainActivity.kt
@@ -2,8 +2,21 @@ package com.learn.bookhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.circularreveal.coordinatorlayout.CircularRevealCoordinatorLayout
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.widget.FrameLayout
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
